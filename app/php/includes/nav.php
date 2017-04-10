0dc7659526e3475f3c6562f4642cbb86cfec5606
Dmitri Marov <dimavanguard@gmail.com>
2017-04-10 16:51:05 +0300

    Working on nav

--- app/php/includes/nav.php
+++ app/php/includes/nav.php
@@ -1,8 +1,16 @@
 <nav class="nav">
-	<h1 class="title">SOULmate</h1>
-	<ul class="nav__list">
-		<li class="nav__item">item 1</li>
-		<li class="nav__item">item 2</li>
-		<li class="nav__item">item 3</li>
-	</ul>
+	<div class="container">
+		<h1 class="nav__title">SOULmate</h1>
+		<ul class="nav__list">
+			<a href="#" class="nav__link"><li class="nav__item">item 1</li></a>
+			<a href="#" class="nav__link"><li class="nav__item">item 2</li></a>
+			<a href="#" class="nav__link"><li class="nav__item">item 3</li></a>
+		</ul>
+		<div class="profile">
+			<ul class="profile__list">
+				<a href="#" class="profile__link"><li class="profile__item">Войти</li></a>
+				<a href="#" class="profile__link"><li class="profile__item">Зарегестрироваться</li></a>
+			</ul>
+		</div>
+	</div>
 </nav>
