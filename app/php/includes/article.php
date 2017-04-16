c7e8b093f0d3d5b801c37e29fb9e85ca277389a2
Dmitri Marov <dimavanguard@gmail.com>
2017-04-16 15:17:40 +0300

    Working on profile. Add article, about. About and user have min-height

---
+++ app/php/includes/article.php
@@ -0,0 +1,12 @@
+<article class="article thumbnail">
+	<div class="article__author">
+		<div class="author__pic"><img src="img/content/ava.jpg" alt="Author pic"></div>
+		<div class="author__text">
+			<h2 class="author__name">NAME</h2> <br>	
+			<h3 class="author__post-date">
+				<time timedate="DATE">DATE</time>
+			</h3>
+		</div>
+	</div>
+	.article__text
+</article>
--- app/php/profile.php
+++ app/php/profile.php
@@ -10,13 +10,34 @@
 			?>
 			
 			<div class="container">
-				<div class="profile">
-					<div class="profile__pic">
-						<img src="img/content/ava.jpg" alt="My photo">
+				<main class="profile">
+					<div class="profile__left">
+						<section class="user thumbnail">
+							<div class="user__pic">
+								<img src="img/content/ava.jpg" alt="My photo">
+							</div>
+							<button class="user__btn">Редактировать</button>
+						</section>
 					</div>
-				</div>
-			</div>
+					
+					<div class="profile__right">
+						<section class="about thumbnail">
+							<h2 class="about__title">Имя</h2>
+							<hr class="about__line">
+							<div class="about__info">
+								<h3 class="about__label">Родной город:</h3>
+								<h3 class="about__label">Девиз:</h3>
+							</div>
+							<hr class="about__line">
+						</section>	
+					</div>
+				</main>
 
+				<section class="history">
+					<?php include_once "includes/article.php"; ?>
+				</section>
+
+			</div>
 		</div>
 	</body>
 </html>
